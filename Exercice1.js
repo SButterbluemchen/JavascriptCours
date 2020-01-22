c06e0151fbcea42c185125426c252a0f0c5196e3
Sina Brauch <you@example.com>
2020-01-22 15:08:25 +0100

    Exco 1 OC

--- Exercice1.js
+++ Exercice1.js
@@ -1,2 +1,36 @@
-const toto = 10;
-console.log(toto);
+// Chapitre 1 - Exercice 1 
+console.log("Bonjour Damien !");
+
+// Chapitre 1 - Exercice 2
+console.log("Bonjour en JavaScript !");
+// Cette ligne est un commentaire : elle n'est pas exécutée
+console.log("Faisons quelques calculs.");
+console.log(4 + 7);
+console.log(12 / 0);
+console.log("Au revoir !");
+
+// Chapitre 1 - Exercice 3
+console.log("My name is Sina.");
+console.log("I am 29 years old.");
+
+// Chapitre 1 - Exercice 4
+console.log(6+3);
+console.log(6-3);
+console.log(6*3);
+console.log(6/3);
+
+// Chapitre 1 - Exercice 5
+console.log(4 + 5); //9
+console.log("4 + 5"); //4+5
+console.log("4" + "5"); //45
+
+// Chapitre 2 - Exercice 1
+let a;
+a = 3.14;
+console.log(a);
+
+let a = 3.14;
+console.log(a);
+
+const a = 3.14; // Création d'une variable constante
+a = 6.28;       // Impossible : a ne peut pas changer de valeur !
